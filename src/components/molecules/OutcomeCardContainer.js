ffc9d7e4d6bc9e18cf2fef6913406eca293c8939
ben <Astwood>
2019-04-19 18:21:27 +0100

    Removed unnecessary Divider component import

--- src/components/molecules/OutcomeCardContainer.js
+++ src/components/molecules/OutcomeCardContainer.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import Divider from '@material-ui/core/Divider';
 import {withStyles} from '@material-ui/core/styles';
 import OutcomeCard from '../atoms/OutcomeCard';
 
