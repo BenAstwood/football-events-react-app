e0b57ec4a81243445f8bc8d9aa1bd821460127cf
ben <Astwood>
2019-04-19 13:00:51 +0100

    Refactored header template
    Is now created with material ui components.

--- src/components/templates/Header.js
+++ src/components/templates/Header.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {withStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
-const Header = props => (
-  <header className="top">
-    <h1>Live football</h1>
-  </header>
-);
+const Header = props => {
+  const {classes} = props;
 
-export default Header;
+  return (
+    <AppBar className={classes.root} position="static" color="default">
+      <Toolbar>
+        <Typography variant="h6" color="inherit">
+          Live football
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+};
+
+const styles = {
+  root: {
+    flexGrow: 1,
+    background: '#f44336',
+    color: 'white'
+  }
+};
+
+export default withStyles(styles)(Header);
